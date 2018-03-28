4452040bbe304b22e846ac03b5f3d6ccc1b115ab
agnynureza <rezaagny@gmail.com>
2018-03-28 12:05:34 +0700

    done

--- controllers/books.js
+++ controllers/books.js
@@ -1,3 +1,6 @@
+const Book = require('../models/Book')
+const mongoose = require('mongoose')
+
 module.exports = {
   all: function(req, res) {
     Book.find(function (err, books) {
@@ -16,8 +19,8 @@ module.exports = {
       res.send(result)
     });
   },
-  update: function(req, res) {
-    Book.update({ _id: req.id }, {
+  update: function(req, res) {        
+    Book.update({ _id: mongoose.Types.ObjectId(req.params.id) }, {
       $set: req.body
     }, function(err, result) {
       if (err) {
@@ -27,7 +30,7 @@ module.exports = {
     });
   },
   delete: function(req, res) {
-    Book.remove({ _id: req.id }, function (err, result) {
+    Book.remove({ _id: req.params.id }, function (err, result) {
       if (err) {
         res.send({err: err})
       }
